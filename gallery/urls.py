86f084dc154642c55946a6cf8fca7c81d068f35b
vladimir/yashix <yarishevvova@gmail.com>
2025-12-14 17:09:15 +0300

    добавление возможности входа, инфо-страницы и формы

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+
     path('work/<slug:slug>/', views.WorkDetailView.as_view(), name='work_detail'),
     path('services/', views.ServicesView.as_view(), name='services'),
+
+    # Статические страницы
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('help/', views.HelpView.as_view(), name='help'),
+
+    # Авторизация
+    path('login/', auth_views.LoginView.as_view(
+        template_name='gallery/login.html'
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='home'
+    ), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+
+    # Добавление контента
+    path('category/add/', views.CategoryCreateView.as_view(), name='category_add'),
+    path('work/add/', views.WorkCreateView.as_view(), name='work_add'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from .models import Work, Category, Service
+
 
 class HomeView(ListView):
     model = Work
@@ -18,12 +23,16 @@ class HomeView(ListView):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all().order_by('order', 'name')
         return context
+
+
 class WorkDetailView(DetailView):
     model = Work
     template_name = 'gallery/work_detail.html'
     context_object_name = 'work'
-    slug_field = 'slug'          # по какому полю искать
-    slug_url_kwarg = 'slug'      # как параметр называется в url-паттерне
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+
 class ServicesView(ListView):
     model = Service
     template_name = 'gallery/services.html'
@@ -32,3 +41,53 @@ class ServicesView(ListView):
     def get_queryset(self):
         return Service.objects.filter(is_active=True).order_by('order')
 
+
+# --- Статические страницы ---
+
+class AboutView(TemplateView):
+    template_name = 'gallery/about.html'
+
+
+class HelpView(TemplateView):
+    template_name = 'gallery/help.html'
+
+
+# --- Авторизация / регистрация ---
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'gallery/register.html'
+    success_url = reverse_lazy('home')
+
+
+# --- Миксин для форм добавления контента ---
+
+class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+# --- Формы добавления категорий и работ ---
+
+class CategoryCreateView(StaffRequiredMixin, CreateView):
+    model = Category
+    fields = ['name', 'slug', 'description', 'order']
+    template_name = 'gallery/category_form.html'
+    success_url = reverse_lazy('home')
+
+
+class WorkCreateView(StaffRequiredMixin, CreateView):
+    model = Work
+    fields = [
+        'title',
+        'slug',
+        'category',
+        'preview_image',
+        'description',
+        'created_at',
+        'is_published',
+    ]
+    template_name = 'gallery/work_form.html'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
